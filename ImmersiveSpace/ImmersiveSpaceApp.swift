424acc0ec707516e6a886ae66284976e525952a5
belkhadir <belkhadir.anas@gmail.com>
2025-09-21 16:49:42 +0100

    Initial Commit

---
+++ ImmersiveSpace/ImmersiveSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImmersiveSpaceApp.swift
+//  ImmersiveSpace
+//
+//  Created by belkhadir on 21/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct ImmersiveSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
